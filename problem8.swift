116b27de607c42dc8e4cf71a2d41d7c7a1293a9c
bagdatkamila <123236855+bagdatkamila@users.noreply.github.com>
2024-09-26 21:32:45 +0500

    Delete problem8.swift

--- problem8.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-func isPalindrome(_ text: String) -> Bool{
-    let cleanedText = text.lowercased().filter{ $0.isLetter }
-    
-    return cleanedText == String(cleanedText.reversed()) //reverse our word to chech if it identical
-}
